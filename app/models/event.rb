9bf80791748f3d5928584ba5eb391fee19cd64ce
Diego Monteiro <dmsburn@gmail.com>
2019-04-09 19:44:09 -0500

    Ajustando formulário e mapa para envio de eventos

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :city
   belongs_to :user
+
+  enum type: [:holes, :reclamation , :trash , :dissaster , :assaults ]
+  enum status: [:pendent , :working , :solved ]
+
 end
