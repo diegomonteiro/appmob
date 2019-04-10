cd79365a1a89ec9436bde01ee93765a5fcbb97c5
Diego Monteiro <dmsburn@gmail.com>
2019-04-10 15:31:09 -0500

    Ajustando o acesso e envio de eventos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery prepend: true
 	before_action :configure_devise_params, if: :devise_controller?
-	before_action :authenticate_user!
+	
 	before_action :set_paper_trail_whodunnit
 
 	def user_for_paper_trail
@@ -11,6 +11,5 @@ class ApplicationController < ActionController::Base
   	protected
 	  def configure_devise_params
 	    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :email, :password, :password_confirmation])
-	    
-	end
+	  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
 	def index
+		@events = Event.where(nil)
+
+		@count_holes = 10
+		@count_reclamation = 4
+		@count_trash = 5
+		@count_desaster = 4
+		@count_assault = 10
+		@count_solveds = 6
 	end
 end
